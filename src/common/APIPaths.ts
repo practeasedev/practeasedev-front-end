7a7482d3f49fdccb2bf697491cfa38e7b58dccac
P-Anurag <pasupureddi.anurag@gmail.com>
2023-07-11 20:42:04 +0530

    Project home page initial commit

--- src/common/APIPaths.ts
+++ src/common/APIPaths.ts
@@ -1,9 +1,9 @@
+const API_VERSION_1 = "/api/v1";
 export const BASE_URL_DEV = "http://localhost:5000";
-export const BASE_URL = BASE_URL_DEV
+export const BASE_URL = BASE_URL_DEV;
 
 export const GITHUB_AUTHORIZE = "https://github.com/login/oauth/authorize";
 
-export const LOGIN_OR_REGISTER = '/api/v1/auth/register';
-export const LOGOUT_USER = '/api/v1/auth/logout';
-export const GET_ALL_PROJECTS = '/api/v1/projects';
-
+export const LOGIN_OR_REGISTER = `${API_VERSION_1}/auth/register`;
+export const LOGOUT_USER = `${API_VERSION_1}/auth/logout`;
+export const GET_ALL_PROJECTS = `${API_VERSION_1}/projects`;
