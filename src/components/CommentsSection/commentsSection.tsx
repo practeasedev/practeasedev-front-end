b9b1a6a36c6851e6199b05f7a40a1b589168635a
P-Anurag <pasupureddi.anurag@gmail.com>
2023-10-09 23:41:01 +0530

    Fixing the load more logic

--- src/components/CommentsSection/commentsSection.tsx
+++ src/components/CommentsSection/commentsSection.tsx
@@ -15,7 +15,7 @@ const CommentsSection = ({ projectId }: { projectId: string }) => {
   const [offset, setOffset] = useState<number>(0);
   const [commentText, setCommentText] = useState<string>("");
   const [comments, setComments] = useState<ICommentDetails[]>([]);
-  const [showLoadMore, setShowLoadMore] = useState<boolean>(true);
+  const [showLoadMore, setShowLoadMore] = useState<boolean>(false);
 
   // animations Ref
   const [commentInputRef, commentInputInView] = useInView(INTERSECTION_OBSERVER_OPTIONS);
@@ -36,6 +36,10 @@ const CommentsSection = ({ projectId }: { projectId: string }) => {
     if (offset > 0 && data.length === 0) {
       toast.success("No more comments to load", { duration: 2000 });
       setShowLoadMore(false);
+    }else if(data.length < 5){
+      setShowLoadMore(false);
+    }else{
+      setShowLoadMore(true);
     }
     setComments((commentsArray) => [...commentsArray, ...data]);
   };
